d71c6af9a7d25a3bf2a22de9db83f8f4e9f50c32
Putra Surya <putrapradesa@gmail.com>
2019-12-03 11:33:33 +0700

    Update route.js

--- src/routes/master.js
+++ src/routes/master.js
@@ -216,7 +216,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "master",
-            //permission: { "C9": 1 },
+            permission: { "U11": 1 },
             iconClass: 'fa fa-dashboard'
         }
     },
