a4712d4123b1292dff741ffb3c959293f4aa4681
putrapradesa <49633721+putrapradesa@users.noreply.github.com>
2020-06-06 12:01:20 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -46,7 +46,7 @@ export async function configure(aurelia) {
       //var customsReport = "https://com-danliris-service-support.azurewebsites.net/v1/";
       //var merchandiser = "https://com-danliris-service-md.azurewebsites-dev.net/v1/";
       //const dealTracking = 'https://com-danliris-service-deal-tracking-dev.azurewebsites.net/v1/';
-      const sales = "https://com-mas-sales-dev.azurewebsites.net/v1/";
+      const sales = "https://com-mas-service-sales.azurewebsites.net/v1/";
       //var weaving = "https://com-danliris-service-weaving.azurewebsites.net/";
       //var finance = "https://com-danliris-service-finance-accounting.azurewebsites.net/v1/";
       //var garmentProduction = "https://com-danliris-service-garment-dev.azurewebsites.net/";
